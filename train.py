d11723c8107bb32bc307876dd8dc800f473670c6
COOL3163 <elliott.dong.2009@outlook.com>
2025-10-09 16:59:22 +0800

    initialization of weights and biases and forward pass function

--- train.py
+++ train.py
@@ -1,6 +1,27 @@
 import numpy as np
 import pickle
 
+def sigmoid(x):
+    """
+    Sigmoid activation function
+    """
+    return 1 / (1 + np.exp(-x))
+
+def relu(x):
+    """
+    ReLU activation function
+    Use RelU for this ANN
+    """
+    return np.maximum(0, x)
+
+def softmax(z):
+    """
+    Softmax activation function
+    """
+    max_z = np.max(z, axis=0, keepdims=True)  
+    exp_z = np.exp(z - max_z)
+    return exp_z / np.sum(exp_z, axis=0, keepdims=True)
+
 class NeuralNetwork:
     def __init__(self, sizes):
         """
@@ -9,12 +30,40 @@ class NeuralNetwork:
         self.sizes = sizes
         self.num_layers = len(sizes)
         # add weight and bias initialization here
+        self.weights = []
+        self.biases = []
+        
+        for i in range(self.num_layers - 1):
+            x = self.sizes[i] 
+            y = self.sizes[i + 1] # neurons in next layer
+            self.weights.append(np.random.randn(y, x) * np.sqrt(2 / x)) # Kaiming/He initialization
+            self.biases.append(np.zeros((y, 1))) # all zeros
+            
+        
 
     def forward(self, x):
         """
         Do a forward pass through the network
         """
-        pass
+        self.activations = [x.T]  # store activations for backpropagation (transposed input)
+        self.z_values = []        # store z values for backpropagation
+
+        # Hidden layers (all but last)
+        for w, b in zip(self.weights[:-1], self.biases[:-1]):
+            z = np.dot(w, self.activations[-1]) + b
+            self.z_values.append(z)
+            a = relu(z)
+            self.activations.append(a)
+
+        # Output layer (softmax)
+        w_out = self.weights[-1]
+        b_out = self.biases[-1]
+        z_out = np.dot(w_out, self.activations[-1]) + b_out
+        self.z_values.append(z_out)
+        output = softmax(z_out)   # shape (n_classes, batch_size)
+        self.activations.append(output)
+
+        return output
 
     def backward(self, x, y, learning_rate):
         """
@@ -40,6 +89,7 @@ class NeuralNetwork:
 if __name__ == "__main__":
     sizes = [] # layer size
     nn = NeuralNetwork(sizes) # initialize the neural network
+    
 
     nn.save('model.pkl') # save the model
 
